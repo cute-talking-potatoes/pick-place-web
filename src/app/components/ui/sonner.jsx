e47f65f6b978810d4faaf98d52636f7e95d45064
naayoung <ask5046a@gmail.com>
2026-03-15 17:32:09 +0900

    Chore: 버전 업데이트

--- src/app/components/ui/sonner.jsx
+++ src/app/components/ui/sonner.jsx
@@ -1,10 +1,8 @@
 "use client";
-import { useTheme } from "next-themes";
 import { Toaster as Sonner } from "sonner";
 const Toaster = ({ ...props }) => {
-  const { theme = "system" } = useTheme();
   return <Sonner
-    theme={theme}
+    theme="system"
     className="toaster group"
     style={{
       "--normal-bg": "var(--popover)",
